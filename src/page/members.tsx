b99382c9c170e15f4d199adad3203a829cdf3406
信原 有志 <ynobuhar@yahoo-corp.jp>
2023-09-10 17:18:04 +0900

    不要なログ削除

--- src/page/members.tsx
+++ src/page/members.tsx
@@ -27,7 +27,6 @@ const MemberProfile: React.FC<MemberProfileProps> = (props) => {
     return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
   }
 
-  console.log(props.member)
   return (
     <Card className="mb-4">
       <div className="d-flex align-items-start p-3">
